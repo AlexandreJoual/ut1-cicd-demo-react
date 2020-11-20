fa52cb853dbb1cb5552be645811e7a2278d22b45
Alex Joual <Alexandre.joual@gmail.com>
2020-11-20 08:26:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello UT1 students!
+          Hello UT1 students - From the demo !
         </p>
       </header>
     </div>
