89a15ff496e1efd636d142732c6b4390f0a39b61
Alex Joual <Alexandre.joual@gmail.com>
2020-11-20 13:04:49 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('returns from demo text', () => {
+test('returns hello text', () => {
   render(<App />);
   const linkElement = screen.getByText(/hello/i);
   expect(linkElement).toBeInTheDocument();
